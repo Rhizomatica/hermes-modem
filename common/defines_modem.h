b2ae0bb31571a13c12743aa9d1eba66191287cbb
Rafael Diniz <rafael@riseup.net>
2025-08-12 02:14:26 +0100

    Clarify licenses

--- common/defines_modem.h
+++ common/defines_modem.h
@@ -1,3 +1,22 @@
+/* HERMES Modem
+ *
+ * Copyright (C) 2025 Rhizomatica
+ * Author: Rafael Diniz <rafael@riseup.net>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef HAVE_DEFINES_MODEM_H
 #define HAVE_DEFINES_MODEM_H
